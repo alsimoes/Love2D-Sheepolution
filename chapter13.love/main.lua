6b7d022dc234fd629706224983d716be7121ba5a
André Simões <alsimoes@users.noreply.github.com>
2020-11-02 12:44:23 -0300

    rectangles sides

--- chapter13.love/main.lua
+++ chapter13.love/main.lua
@@ -8,12 +8,27 @@ function love.load()
         width = 100,
         height = 100
     }
+
     r2 = {
         x = 250,
         y = 120,
         width = 150,
         height = 120
     }
+end
+
+function checkCollision(a, b)
+    local a_left = a.x
+    local a_right = a.x + a.width
+    local a_top = a.y
+    local a_botton = a.y + a.height
+
+    local b_left = b.x
+    local b_right = b.x + b.width
+    local b_top = b.y
+    local b_botton = b.y + b.height
+
+    
 end
 
 function love.update(dt)
