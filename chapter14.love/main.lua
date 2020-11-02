9d6e99a9932b90aa90fccba2fccbf4d38421e266
André Simões <alsimoes@users.noreply.github.com>
2020-11-02 16:42:25 -0300

    ...and there is an enemy!

--- chapter14.love/main.lua
+++ chapter14.love/main.lua
@@ -5,14 +5,18 @@
 function love.load()
     Object = require("lib.classic")
     require "lib.player"
+    require "lib.enemy"
 
     player = Player()
+    enemy = Enemy()
 end
 
 function love.update(dt)
     player:update(dt)
+    enemy:update(dt)
 end
 
 function love.draw()
     player:draw()
+    enemy:draw()
 end
