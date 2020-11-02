15bb63807727f9b16a1b3e8cb1be1b3828a8c0b1
André Simões <alsimoes@users.noreply.github.com>
2020-11-02 16:55:47 -0300

    Bullets added

---
+++ chapter14.love/lib/bullet.lua
@@ -0,0 +1,18 @@
+--! file: bullet.lua
+--
+-- Bullet class definitions
+--
+
+Bullet = Object:extend()
+
+function Bullet:new()
+    
+end
+
+function Bullet:update(dt)
+    
+end
+
+function Bullet:draw()
+    
+end
--- chapter14.love/main.lua
+++ chapter14.love/main.lua
@@ -6,17 +6,22 @@ function love.load()
     Object = require("lib.classic")
     require "lib.player"
     require "lib.enemy"
+    require "lib.bullet"
 
     player = Player()
     enemy = Enemy()
+    lsitOfBullets = {}
+
 end
 
 function love.update(dt)
     player:update(dt)
     enemy:update(dt)
+    bullet:update(dt)
 end
 
 function love.draw()
     player:draw()
     enemy:draw()
+
 end
